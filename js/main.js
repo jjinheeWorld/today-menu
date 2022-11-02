1c0c479a1d69b858a2c8a27da956d4853374de5b
jjinheeWorld <wlsgml450@naver.com>
2022-11-03 03:24:47 +0900

    추가 : 추천 버튼 이벤트 작업

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ function todayMenu(data) {
   // 강수형태
   let ptyData = data;
 }
+
+// 버튼 이벤트
+(function () {
+  const btn = document.querySelector('.today-menu > button');
+  const lightBox = document.querySelector('.today-menu-lightbox');
+  const pickMenu = document.querySelector('.pick-menu');
+
+  btn.addEventListener('click', showPickMenu);
+
+  // 추천 메뉴 화면 보이기
+  function showPickMenu() {
+    lightBox.classList.add('on');
+    pickMenu.classList.add('on');
+  }
+  // 추천 메뉴 화면 닫기
+  lightBox.addEventListener('click', closePickMenu);
+  function closePickMenu() {
+    lightBox.classList.remove('on');
+    pickMenu.classList.remove('on');
+  }
+})();
