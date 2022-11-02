d53dba82e78a7df1ea38806f01348cc6c894cbfa
jjinheeWorld <wlsgml450@naver.com>
2022-11-03 03:08:19 +0900

    추가 : 미세먼지 모듈 내보내기

--- js/getData.js
+++ js/getData.js
@@ -71,3 +71,75 @@ export function getWeather() {
   }
   return weatherResult;
 }
+
+// 미세먼지 정보 내보내기
+export function getPM() {
+  // 실시간 미세먼지 정보 API
+  const pMResult = fetch(
+    'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=cbKPAKuWx1NutItr9hyyQeTi1Sx%2BBmKYKNE2khzyNrvHABB0vA7hVL8dV1X3zJFT3X3jpUMmihKQ4d%2FQlqaQZA%3D%3D&returnType=json&numOfRows=100&pageNo=1&stationName=대왕판교로(백현동)&dataTerm=DAILY&ver=1.3'
+  )
+    .then((response) => response.json())
+    .then(getPMData);
+
+  // PM10 = 미세먼지
+  // pm10Value = 미세먼지 농도
+  // pm10Grade1h = 미세먼지 1시간 등급
+  // Grade 값 = 1(좋음), 2(보통), 3(나쁨), 4(매우나쁨)
+  function getPMData(data) {
+    const updatePMData = data.response.body.items[0]; // 최신 미세먼지 정보
+
+    const pMImg = document.querySelector('.pm');
+    const pMGrade = document.querySelector('.pm-grade');
+    const pMValue = document.querySelector('.pm-value');
+
+    // resize될 때 마다 이미지 색상 변경
+    window.addEventListener('resize', viewPMImg);
+
+    // 미세먼지 농도
+    pMValue.textContent = `${updatePMData.pm10Value} ㎍/㎥`;
+
+    // 미세먼지 등급
+    viewPMGrade();
+    function viewPMGrade() {
+      if (updatePMData.pm10Grade1h == 1) {
+        pMGrade.textContent = '좋음';
+      } else if (updatePMData.pm10Grade1h == 2) {
+        pMGrade.textContent = '보통';
+      } else if (updatePMData.pm10Grade1h == 3) {
+        pMGrade.textContent = '나쁨';
+      } else {
+        pMGrade.textContent = '매우나쁨';
+      }
+    }
+    // viewPMImg 함수에서 사용하기 위해 pMGrade.textContent 값을 변수에 담는다.
+    const pMGradeText = pMGrade.textContent;
+
+    // 미세먼지 등급에 따른 이미지
+    viewPMImg();
+    function viewPMImg() {
+      if (window.innerWidth < 989) {
+        if (pMGradeText == '좋음') {
+          pMImg.style.backgroundImage = `url(../images/emoji/very-good.svg)`;
+        } else if (pMGradeText == '보통') {
+          pMImg.style.backgroundImage = `url(../images/emoji/good.svg)`;
+        } else if (pMGradeText == '나쁨') {
+          pMImg.style.backgroundImage = `url(../images/emoji/bad.svg)`;
+        } else {
+          pMImg.style.backgroundImage = `url(../images/emoji/very-bad.svg)`;
+        }
+      } else {
+        if (pMGradeText == '좋음') {
+          pMImg.style.backgroundImage = `url(../images/emoji/very-good-black.svg)`;
+        } else if (pMGradeText == '보통') {
+          pMImg.style.backgroundImage = `url(../images/emoji/good-black.svg)`;
+        } else if (pMGradeText == '나쁨') {
+          pMImg.style.backgroundImage = `url(../images/emoji/bad-black.svg)`;
+        } else {
+          pMImg.style.backgroundImage = `url(../images/emoji/very-bad-black.svg)`;
+        }
+      }
+    }
+    return updatePMData.pm10Grade1h;
+  }
+  return pMResult;
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 import { getWeather } from './getData.js';
 // import { getPM } from './getData.js';
+
+getWeather().then(todayMenu);
+
+function todayMenu(data) {
+  // 강수형태
+  let ptyData = data;
+}
